db3c4133b2c8f7e1a2cd3390b2398fe2a1f01e09
OmniPot <r.conkov@gmail.com>
2014-10-26 16:03:28 +0200

    uafaef
    wejfwjef

--- js/contentSlider.js
+++ js/contentSlider.js
@@ -1,10 +1,11 @@
+
 function selectStep(n) {
     if (n == 1) {
         $(".progressSelected").animate({marginLeft: '0'}, 300);
     } else if (n == 2) {
-        $(".progressSelected").animate({marginLeft: '2%'}, 300);
+        $(".progressSelected").animate({marginLeft: '23px'}, 300);
     } else if (n == 3) {
-        $(".progressSelected").animate({marginLeft: '4%'}, 300);
+        $(".progressSelected").animate({marginLeft: '46px'}, 300);
     }
     $(".contentSwitcher").hide();
     $("#content"+n).show();
